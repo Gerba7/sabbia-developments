ea8126a710660959d46f4514e10536b46e9530cf
Gerba <gmlongobardi@hotmail.com>
2024-04-16 11:09:51 -0300

    V2.5

--- app/servicios/page.js
+++ app/servicios/page.js
@@ -1,14 +1,17 @@
 import PageCover from '../ui/components/Common/PageCover/PageCover';
 import ServiceCover from '../../public/images/serviceCover.jpeg';
 import Header from '../ui/components/Common/Header/Header';
+import PageCover2 from '../ui/components/Common/PageCover/PageCover2';
 
 
 
 const Services = () => {
   return (
     <div>
-      <PageCover img={ServiceCover} />
-      <Header title={'SERVICIOS'} />
+      {/* <PageCover img={ServiceCover} />
+      <Header title={'SERVICIOS'} /> */}
+      <PageCover2 title={'SERVICIOS'} />
+      <div style={{height: '50vh'}}></div>
     </div>
   )
 }
