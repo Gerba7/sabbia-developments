cf8910bad0417f7dc2fe5119c8f0d6437693dea2
Gerba <gmlongobardi@hotmail.com>
2024-04-24 15:19:10 -0300

    V2.7

--- app/servicios/page.js
+++ app/servicios/page.js
@@ -1,17 +1,61 @@
-import PageCover from '../ui/components/Common/PageCover/PageCover';
-import ServiceCover from '../../public/images/serviceCover.jpeg';
-import Header from '../ui/components/Common/Header/Header';
 import PageCover2 from '../ui/components/Common/PageCover/PageCover2';
+import styles from '../ui/components/Services/servicios.module.css';
+import House from '../../public/images/house.svg';
+import Blueprint from '../../public/images/blueprint.svg';
+import Architect from '../../public/images/architect.svg';
+import Image from 'next/image';
 
 
 
 const Services = () => {
   return (
     <div>
-      {/* <PageCover img={ServiceCover} />
-      <Header title={'SERVICIOS'} /> */}
       <PageCover2 title={'SERVICIOS'} />
-      <div style={{height: '50vh'}}></div>
+      <div className={styles.wrapper}>
+        <div className={styles.serviceContainer}>
+          <div className={styles.icon}>
+            <Image src={House} alt='house' width={50} height={50} />
+          </div>
+          <h5 className={styles.title}>DESARROLLO</h5>
+          <hr className={styles.hr} />
+          <p className={styles.description}>
+            Ofrecemos una búsqueda meticulosa de propiedades y terrenos, 
+            acompañada de un análisis de factibilidad exhaustivo, asesoramiento 
+            integral y gestión de obras para inversión. Nos comprometemos a 
+            identificar oportunidades que se alineen con los objetivos de nuestros 
+            clientes, asegurando que cada proyecto supere las expectativas y metas 
+            establecidas.
+          </p>
+        </div>
+        <div className={styles.serviceContainer}>
+          <div className={styles.icon}>
+            <Image src={Blueprint} alt='house' width={50} height={50} />
+          </div>
+          <h5 className={styles.title}>PROYECTO Y DIRECCIÓN</h5>
+          <hr className={styles.hr} />
+          <p className={styles.description}>
+            Nuestro equipo se encarga del diseño integral y la elaboración de planos 
+            detallados, garantizando una ejecución impecable de cada obra. Mediante 
+            una planificación financiera detallada, anticipamos todos los costos y plazos 
+            de realización, minimizando cualquier eventualidad. Nos distinguimos por un 
+            seguimiento meticuloso de cada proyecto y una comunicación constante y efectiva 
+            con nuestro equipo técnico.
+          </p>
+        </div>
+        <div className={styles.serviceContainer}>
+          <div className={styles.icon}>
+            <Image src={Architect} alt='house' width={50} height={50} />
+          </div>
+          <h5 className={styles.title}>CONSTRUCCIÓN</h5>
+          <hr className={styles.hr} />
+          <p className={styles.description}>
+            Mantenemos una presencia activa en cada fase de la construcción, ejerciendo un 
+            control riguroso sobre la calidad de los materiales y su utilización óptima. 
+            Prestamos especial atención a los detalles y acabados finales, asegurando la completa 
+            satisfacción de nuestros clientes.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- app/ui/components/Home/HowWeWork/HowWeWork.jsx
+++ app/ui/components/Home/HowWeWork/HowWeWork.jsx
@@ -8,7 +8,7 @@ const HowWeWork = () => {
   return (
     <div className={styles.container}>
       <div className={styles.titleContainer}>
-        <h3 className={styles.title}>COMO TRABAJAMOS</h3>
+        <h3 className={styles.title}>EMPRESA</h3>
         <div className={styles.hr}></div>
       </div>
       <div className={styles.cardsContainer}>
